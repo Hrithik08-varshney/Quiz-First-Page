ef83d13a917f0b3dca85f3cf5ea2b608823ed04c
Hrithik Varshney <83474285+Hrithik08-varshney@users.noreply.github.com>
2022-01-25 20:11:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+/* export  const val=document.querySelector('#input').value; */
 const btn=document.querySelector('.btn');
-let arr=[];
-let sr=0;
+/* let arr=[]; */
 btn.addEventListener("click",function(){
+    let key=Object.keys(localStorage); 
+    key.sort();
+    console.log(key);     //taking all keys as an array
+    if(key.length==0){
+        sr=0;
+    } else{                     
+    sr=parseInt(key[key.length-1]); 
+    console.log(sr);  //assingning last element of array to sr
+    }             
     sr++;
     const val=document.querySelector('#input').value;
     let details={
@@ -9,8 +18,9 @@ btn.addEventListener("click",function(){
         yname:val,
         score:0
     }
-    arr.push(details);
-    localStorage.setItem(details.yname,JSON.stringify(arr));
+    /* arr.push(details); */
+    /* localStorage.setItem(details.yname,JSON.stringify(details)); */
+    localStorage.setItem(sr,JSON.stringify(details));
     document.querySelector('#input').value=" ";
 })
 /* console.log(arr); */
